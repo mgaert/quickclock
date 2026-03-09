b3fb5b4841fb2320f40c5a6ae22d90320443feea
Michael Gärtner <mgaert@gmx.de>
2026-03-09 23:21:27 +0100

    Suppress unchecked cast warning in ViewModelProvider.Factory
    - Add @Suppress("UNCHECKED_CAST") annotation to Factory.create()
    - Resolves compiler warning for safe type cast
    - Build now completes with zero warnings

--- android/app/src/main/kotlin/com/quickclock/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/quickclock/app/MainActivity.kt
@@ -36,6 +36,7 @@ class MainActivity : ComponentActivity() {
         ViewModelProvider(
             this,
             object : ViewModelProvider.Factory {
+                @Suppress("UNCHECKED_CAST")
                 override fun <T : ViewModel> create(modelClass: Class<T>): T {
                     return WorktimeViewModel(applicationContext) as T
                 }
